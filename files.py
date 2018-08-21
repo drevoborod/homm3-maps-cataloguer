e905a025344ba94ddb2c42cae969a668c6dd8cbf
drevoborod <drevoborod@gmail.com>
2018-08-21 13:57:43 +0300

    Project structure in development.

--- files.py
+++ files.py
@@ -3,19 +3,25 @@ import os
 import gzip
 import zlib
 
+import parser
 
-class FilesOperations:
+
+class IncorrectMapFileError(Exception): pass
+
+
+class FilesAggregator:
     """Find maps in directory."""
     def __init__(self, input_path):
         self.path = input_path
+        self.maps_dict = []
 
     def move(self):
         """Move file to new place."""
         pass
 
     def walker(self):
-        """Create a list of map files."""
-        with os.scandir(self.mappath) as path:
+        """Create a list of directory files."""
+        with os.scandir(self.path) as path:
             for element in path:
                 if element.is_file():
                     pass
@@ -27,15 +33,29 @@ class FilesOperations:
             except Exception:
                 sys.exit("Cannot create output directory.")
 
+    def maps_dict_prepare(self):
+        files = self.walker()
+        for file in files:
+            f = MapFile(file)
+            f.check()
+            file_data = parser.MapObject(f.unpack())
+            file_data.parse_map()
+            self.maps_dict[file_data.map_name] = file_data.map_contents
+
 
 class MapFile:
     """Unpack map file and return its contents. Write map file to another place if necessary."""
     def __init__(self, filename):
         self.filename = filename
 
-    def check(self):
-        """Check if file is a valid HOMM3 map file."""
+    def check(self, file):
+        """Check if file is a valid HOMM3 map file. If not, raises IncorrectMapFileError."""
         pass
 
     def unpack(self):
-        pass
+        try:
+            f = gzip.open(self.filename, "r")
+        except Exception:
+            raise IncorrectMapFileError
+        else:
+            return f
--- parser.py
+++ parser.py
@@ -1,4 +1,8 @@
 class MapObject:
-    """Prepares full information about map file. Accepts file object of unzipped map file."""
+    """Prepares full information about map file. Accepts file object of unpacked map file."""
     def __init__(self, mapfile):
+        self.map_name = ""
+        self.map_contents = {}
+
+    def parse_map(self):
         pass
