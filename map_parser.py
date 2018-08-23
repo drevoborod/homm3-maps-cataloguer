01890b88a81489d2f640fd6870046a65e3f24ecb
drevoborod <drevoborod@gmail.com>
2018-08-23 08:27:59 +0300

    Parse fixed: support of maps without heroes.

--- map_parser.py
+++ map_parser.py
@@ -22,28 +22,33 @@ class MapObject:
         self.map_type = self.compare_data(4, constants.MAP_TYPE)
         #### Insert check of 01/00: if 00, read +5, else +6
         #### Also check how it will be for non-HotA maps!
-
-        # Get map size:
+        # Determine where to start reading next data:
+        hero_exists = self.bytes_to_dec(1)
         if self.map_type == "HotA":
-            self.mapfile.read(7)
-        else:
-            self.mapfile.read(1)
-
-        ### Refactor to correct usage of all 4 bytes!
-        self.map_size = self.compare_data(1, constants.MAP_SIZE)
-        self.mapfile.read(3)
+            if hero_exists:
+                self.mapfile.read(6)
+            else:
+                self.mapfile.read(4)
+        # Get map size:
+        self.map_size = self.bytes_to_dec(4)
         # Does map have a dungeon?
-        levels = self.bytes_to_dec(self.mapfile.read(1))
+        levels = self.bytes_to_dec(1)
         if levels == 0:
             self.has_dungeon = False
         elif levels == 1:
             self.has_dungeon = True
         # Get map name:
-        length = self.bytes_to_dec(self.mapfile.read(4))
+        length = self.bytes_to_dec(4)
         self.map_name = self.mapfile.read(length).decode("Ansi")
         # Get map description:
-        length = self.bytes_to_dec(self.mapfile.read(4))
+        ### ToDo: Support 0-length description.
+        length = self.bytes_to_dec(4)
         self.map_description = self.mapfile.read(length).decode("Ansi")
+
+        print(self.map_size)
+        print(self.map_name)
+        print(self.map_description)
+        print("Has dungeon:", self.has_dungeon)
 
     def compare_data(self, length, template_dict):
         prepared_template = {binascii.unhexlify(key): template_dict[key] for key in template_dict}
@@ -53,9 +58,9 @@ class MapObject:
         else:
             return prepared_template[data]
 
-    def bytes_to_dec(self, data):
+    def bytes_to_dec(self, length):
         """Converts bytes to hex, and hex - to decimal integer."""
-        data_prepared = bytearray(data)
-        data_prepared.reverse()
-        to_hex = binascii.hexlify(data_prepared)
+        data = bytearray(self.mapfile.read(length))
+        data.reverse()
+        to_hex = binascii.hexlify(data)
         return int(to_hex, 16)
